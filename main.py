5a7fff85359cfc8b6d28063c37900de97cb655e6
bj-wangxing <45442343+bj-wangxing@users.noreply.github.com>
2020-12-28 19:22:22 +0800

    MakeCode 项目的初始文件

---
+++ main.py
@@ -0,0 +1,5 @@
+def forever2():
+    basic.show_icon(IconNames.HEART)
+    basic.pause(100)
+    basic.show_icon(IconNames.HAPPY)
+forever2()
---
+++ main.ts
@@ -0,0 +1,8 @@
+function forever2 () {
+    while (true) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.Happy)
+    }
+}
+forever2()
